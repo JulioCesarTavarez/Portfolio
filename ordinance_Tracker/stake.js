888f10b3aa9c770646b1ad5653e30824e23494c5
JulioCesarTavarez <145470440+JulioCesarTavarez@users.noreply.github.com>
2025-02-04 11:57:20 -0700

    adding images

--- ordinance_Tracker/stake.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const baptism = document.getElementById("baptism");
-
-
-    baptism.addEventListener("check", () => {
-        const bapInput = document.getElementById("bapInput");
-        bapInput.classList.toggle("hide");
-    }); 
-})
